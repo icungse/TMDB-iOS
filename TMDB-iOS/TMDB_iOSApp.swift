afde622fcf4748f6b2e4c6cace4b35993f949c0b
Icung <saiful-islam@outlook.co.id>
2023-08-03 22:11:35 +0700

    inital commit

---
+++ TMDB-iOS/TMDB_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDB_iOSApp.swift
+//  TMDB-iOS
+//
+//  Created by Icung on 03/08/23.
+//
+
+import SwiftUI
+
+@main
+struct TMDB_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
